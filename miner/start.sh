ede2ce66c0e4d387fb2fd02d6e03daaa895bd80f
Lane Rettig <lanerettig@gmail.com>
2017-11-26 18:16:15 +0800

    Remove accounts and privkeys for miner & validator
    Generate random privkeys instead. Replace long entrypoint with start.sh
    script. Receive BOOTSTRAP_NODE via docker env variable.

---
+++ miner/start.sh
@@ -0,0 +1,10 @@
+echo "BOOTSTRAP_NODE is $BOOTSTRAP_NODE"
+if [ -z "$BOOTSTRAP_NODE" ]; then echo "BOOTSTRAP_NODE must be set" && exit 1; fi
+head -1 /dev/random | shasum -a 256 > /root/.config/pyethapp/privkey.hex
+export PRIVKEY=`cat /root/.config/pyethapp/privkey.hex | awk '{print $1}'`
+echo "Generated random private key: $PRIVKEY" 
+perl -pi -e "s/PRIVKEY/$PRIVKEY/" /root/.config/pyethapp/config.yaml
+echo "Creating new account"
+/usr/local/bin/pyethapp --password /root/.config/pyethapp/password.txt account new
+echo "Launching node"
+/usr/local/bin/pyethapp -m 50 --password /root/.config/pyethapp/password.txt -l :info,eth:debug,pow:debug --log-file /root/log/log.txt -b $BOOTSTRAP_NODE run 
