9ab1ba0358f6bfbd700a7b4cf2b10fb9c9fa9eb2
Chih Cheng Liang <chihchengliang@gmail.com>
2017-12-31 18:15:33 +0800

    prettify

--- script/new-account.sh
+++ script/new-account.sh
@@ -2,18 +2,18 @@ KEYSTORE_PATH=$1
 PWD_PATH=$2
 CURRENT_DIR=$3
 
-echo "\n🌟 Creating keystore directory at $KEYSTORE_PATH\n"
+printf "\n🌟 Creating keystore directory at $KEYSTORE_PATH\n"
 mkdir -p $KEYSTORE_PATH
 if [ ! -f $PWD_PATH ]; then
     echo -n "🌟 Enter a new password to encrypt your account:"
     read -s pwd;
-    echo "$pwd" > $PWD_PATH
-    echo "\n🌟 Your password is stored at $PWD_PATH\n"
+    printf "$pwd" > $PWD_PATH
+    printf "\n🌟 Your password is stored at $PWD_PATH\n"
 else
-    echo "\n🌟 Will encrypt your account with $PWD_PATH"
+    printf "\n🌟 Will encrypt your account with $PWD_PATH"
 fi
 
-echo "\n🌟 Pyethapp container is creating new address for you, might take few seconds:\n"
+printf "\n🌟 Pyethapp container is creating new address for you, might take few seconds:\n"
 docker build ./validator -t casper-validator
 docker run -it --rm \
     -v $CURRENT_DIR/validator/data/config:/root/.config/pyethapp \
@@ -21,5 +21,5 @@ docker run -it --rm \
     casper-validator \
     pyethapp --password /root/.config/pyethapp/password.txt account new
 
-echo "\n🌟 New address created at $KEYSTORE_PATH\n"
+printf "\n🌟 New address created at $KEYSTORE_PATH\n"
 ls $KEYSTORE_PATH
